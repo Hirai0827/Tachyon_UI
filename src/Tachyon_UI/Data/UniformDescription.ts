35b8cdf39b3b62b95db7dda44cf4c0d90a212a14
hirai <ryu1999827@gmail.com>
2020-10-08 02:39:35 +0900

    add: description

--- src/Tachyon_UI/Data/UniformDescription.ts
+++ src/Tachyon_UI/Data/UniformDescription.ts
@@ -1,4 +1,11 @@
 export const UniformDescription = {
     time:"the time from beginning of rendering.",
-    resolution:"the resolution of canvas"
+    iTime:"the time from beginning of rendering.",
+    resolution:"the resolution of canvas",
+    iResolution:"the resolution of canvas",
+    press:"1.0 - time from last presssed",
+    good:"1.0 - time from last good button pressed(streaming only)",
+    love:"1.0 - time from last good button pressed(streaming only)",
+    volume:"volume of audio",
+    texture:"texture which you previous set(texture00~texture04)"
 };
